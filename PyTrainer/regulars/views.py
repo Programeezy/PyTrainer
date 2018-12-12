2c8e623cd5a0a3dc3c0e9fa608d58557ca11d32b
Anton Belski <anthony.belski@gmail.com>
2018-12-13 02:17:09 +0300

    The site interface has been changed using the Bootstrap 4 library. Requests to the database have been moved to a separate module. Added tests to the answer.

--- PyTrainer/regulars/views.py
+++ PyTrainer/regulars/views.py
@@ -1,6 +1,6 @@
-import psycopg2
 import random
 import re
+from . import db_methods
 from django.shortcuts import render, HttpResponse
 
 
@@ -9,14 +9,9 @@ def index(request):
 
 
 def get_task(request):
-    conn = psycopg2.connect("dbname=django_db user=anton password=3ie8 host=127.0.0.1")
-    cur = conn.cursor()
-
-    cur.execute('SELECT * FROM task')
-    tasks = cur.fetchall()
+    tasks = db_methods.get_tasks()
     task = random.choice(tasks)
-    cur.execute('SELECT * FROM article')
-    articles = cur.fetchall()
+    articles = db_methods.get_articles()
     first_article = articles[0]
     article = ' '.join(first_article[2])
     article_result = eval(task[3])
@@ -26,10 +21,28 @@ def get_task(request):
                'solution': "def solution(article):\n    # write code here\n    return ''",
                'name': first_article[1],
                'content': first_article[2],
-               'article_result': article_result}
+               'article_result': article_result,
+               'visibility': 'hidden'}
     if request.method == 'POST':
         solution_code = request.POST['solution_code']
         exec(solution_code, globals())
-        context['result'] = solution(article)
         context['solution'] = solution_code
+        if 'run' in request.POST:
+            context['result'] = solution(article)
+        elif 'submit' in request.POST:
+            articles = db_methods.get_articles()
+            for ind, art in enumerate(articles):
+                one_article = articles[ind]
+                article = ' '.join(one_article[2])
+                article_result = eval(task[3])
+                if article_result != solution(article):
+                    context['failed_test'] = str(ind+1) + '/' + str(len(articles))
+                    context['failed_output'] = solution(article)
+                    context['right_output'] = article_result
+                    context['visibility'] = 'visible'
+                    break
+            else:
+                return HttpResponse('woooow!')
     return render(request, 'regulars/task.html', context=context)
+
+
