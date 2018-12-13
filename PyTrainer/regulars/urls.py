60c9a880b769f299623c94c971d35591b4bd275c
Anton Belski <anthony.belski@gmail.com>
2018-12-13 15:06:02 +0300

    Added a page with a task table.

--- PyTrainer/regulars/urls.py
+++ PyTrainer/regulars/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main, name='main'),
     path('task', views.get_task, name='task'),
-    path('history', views.show_history, name='history')
+    path('history', views.show_history, name='history'),
+    path('tasks', views.show_tasks, name='tasks')
 ]
