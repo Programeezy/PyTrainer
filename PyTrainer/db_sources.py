f226dd34fb88efcb40159dc17629dc3147c53d10
TimSmolonski <programeezy@gmail.com>
2019-02-08 17:13:19 +0100

    Added several basic versions of tables for the database. Added test data. Created a simple version of problem solving.

--- PyTrainer/db_sources.py
+++ PyTrainer/db_sources.py
@@ -4,14 +4,24 @@ import psycopg2
 conn = psycopg2.connect("dbname=django_db user=anton password=3ie8 host=127.0.0.1")
 cur = conn.cursor()
 
-cur.execute("""
-  CREATE TABLE IF NOT EXISTS task ( 
-    id SERIAL PRIMARY KEY,
-    status CHAR(32) NOT NULL,
-    description CHAR(256) NOT NULL,
-    solution CHAR(64) NOT NULL);
-""")
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS task ( 
+          id SERIAL PRIMARY KEY,
+          status TEXT NOT NULL,
+          description TEXT NOT NULL,
+          solution TEXT NOT NULL);""")
 
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS article (
+          id SERIAL PRIMARY KEY,
+          name TEXT NOT NULL,
+          content TEXT NOT NULL);""")
+
+# ---Insert into Task-----------------------------------------------
+
+cur.execute(
+    "INSERT INTO task (status, description, solution) VALUES(%s, %s, %s)",
+    ('unsolved', 'Извлекиет все слова в text, начинающиеся на гласную букву', r"re.findall(r'\w+', text)"))
 conn.commit()
 
 cur.close()
--- PyTrainer/regulars/views.py
+++ PyTrainer/regulars/views.py
@@ -1,6 +1,28 @@
-from django.http import Http404
-from django.shortcuts import render
+import psycopg2
+import random
+from django.shortcuts import render, HttpResponse
 
 
 def index(request):
     return render(request, 'regulars/index.html')
+
+
+def get_task(request):
+    conn = psycopg2.connect("dbname=django_db user=anton password=3ie8 host=127.0.0.1")
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM task')
+    tasks = cur.fetchall()
+    task = random.choice(tasks)
+    content = {'task_id': task[0],
+               'status': task[1],
+               'description': task[2],
+               'solution': task[3]}
+
+    if request.method == 'POST':
+        solution = request.POST['solution']
+        if solution == task[3]:
+            return HttpResponse('Solution is correct!')
+        else:
+            return HttpResponse('Solution is not correct!')
+    return render(request, 'regulars/task.html', context=content)
